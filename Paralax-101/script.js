48ab657d3e3c9238fe77aab289820f92706537df
Tom de Monrivall <demonrivalltom@gmail.com>
2021-05-31 16:41:17 +0200

    adding case sensitive png for test

--- Paralax-101/script.js
+++ Paralax-101/script.js
@@ -5,8 +5,8 @@ function parallax (e){
 
     const speed = Layers.getAttribute('data-speed')
 
-    const x= (window.innerWidth - e.pageX*speed)/100
-    const y= (window.innerHeight - e.pageY*speed)/100
+    const x= (window.innerWidth - e.pageX*speed)/70
+    const y= (window.innerHeight - e.pageY*speed)/200
 
     Layers.style.transform=`translateX( ${x}px) translateY( ${y}px) `
 });
